54d1f0041b492345a39716100464895be6827ff9
3wguap <rnbrell@yandex.by>
2026-03-17 20:21:50 +0400

    add model(item)

---
+++ src/main/java/ru/yandex/practicum/mymarket/item/model/Item.java
@@ -0,0 +1,21 @@
+package ru.yandex.practicum.mymarket.item.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "items")
+@Getter
+@Setter
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    private String description;
+    private String imgPath;
+    @Column(nullable = false)
+    private long price;
+}
