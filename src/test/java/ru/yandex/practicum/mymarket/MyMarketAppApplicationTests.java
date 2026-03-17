6d3a06cf2f77bb89a9095201525574b44e5c061b
3wguap <rnbrell@yandex.by>
2026-03-17 19:13:45 +0400

    first commit

---
+++ src/test/java/ru/yandex/practicum/mymarket/MyMarketAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.mymarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMarketAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
