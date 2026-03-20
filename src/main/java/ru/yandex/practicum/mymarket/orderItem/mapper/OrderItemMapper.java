98c6637679685b39d65e43763c2d363618082a82
3wguap <rnbrell@yandex.by>
2026-03-20 15:41:03 +0400

    update orderItem(mapper) and update(repository)

--- src/main/java/ru/yandex/practicum/mymarket/orderItem/mapper/OrderItemMapper.java
+++ src/main/java/ru/yandex/practicum/mymarket/orderItem/mapper/OrderItemMapper.java
@@ -2,12 +2,14 @@ package ru.yandex.practicum.mymarket.orderItem.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import ru.yandex.practicum.mymarket.item.mapper.ItemMapper;
 import ru.yandex.practicum.mymarket.orderItem.dto.OrderItemDto;
 import ru.yandex.practicum.mymarket.orderItem.model.OrderItem;
 
-@Mapper(componentModel = "spring", uses = {ItemMapper.class})
+@Mapper(componentModel = "spring")
 public interface OrderItemMapper {
     @Mapping(source = "item.id", target = "itemId")
+    @Mapping(source = "item.title", target = "title")
+    @Mapping(source = "item.price", target = "price")
+    @Mapping(source = "item.imgPath", target = "imgPath")
     OrderItemDto toOrderItemDto(OrderItem orderItem);
 }
--- src/main/java/ru/yandex/practicum/mymarket/orderItem/repository/OrderItemRepository.java
+++ src/main/java/ru/yandex/practicum/mymarket/orderItem/repository/OrderItemRepository.java
@@ -3,5 +3,5 @@ package ru.yandex.practicum.mymarket.orderItem.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.yandex.practicum.mymarket.orderItem.model.OrderItem;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem,Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 }
